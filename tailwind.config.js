cf2814b60ed98847c9fa81e4e67ac7b6d7bae6e7
mendesjar <ga0301@outlook.com>
2024-08-01 17:51:29 -0300

    feat: init config layout background

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          100: "rgba(4, 0, 54, 0.7)",
+        },
+      },
+      backgroundImage: {
+        main: "radial-gradient(circle, rgba(8,0,116,1) 0%, rgba(4,0,53,1) 100%)",
+      },
+      boxShadow: {
+        main: "inset 0px 0px 10px 0px rgba(255,255,255,1), inset 0px -24px 60px 0px rgba(255,255,255,0.22), inset 0px 8px 24px -16px rgba(255,255,255,0.15);",
+      },
+    },
+  },
+  plugins: [],
+};
