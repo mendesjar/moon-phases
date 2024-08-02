1113a69562806542d71c12b43e6d552cfe0fd735
mendesjar <ga0301@outlook.com>
2024-08-02 17:45:13 -0300

    feat: add phases text

--- src/App.tsx
+++ src/App.tsx
@@ -12,47 +12,58 @@ function App() {
     setRangeValue(value);
   };
   let positions: any = {
-    0: 0,
-    20: 17,
-    40: 34,
-    60: 51,
-    80: 68,
-    100: 85,
-    120: 102,
+    0: { y: 0, phase: "Minguante" },
+    20: { y: 17, phase: "Quarto Minguante" },
+    40: { y: 34, phase: "Gibosa Minguante" },
+    60: { y: 51, phase: "Cheia" },
+    80: { y: 68, phase: "Gibosa Crescente" },
+    100: { y: 85, phase: "Quarto Crescente" },
+    120: { y: 102, phase: "Crescente" },
   };
 
   return (
     <main className="bg-blue-950 h-dvh p-3">
-      <div className="h-full w-full p-5 bg-main rounded-3xl md:rounded-[3rem] shadow-main border-2 border-opacity-65 border-white">
-        <div className="flex justify-center max-h-8 md:max-h-12 h-1/5">
-          <img src={LogoMarca} alt="Logo Marca" />
-        </div>
-        <div className="h-[17rem] flex flex-col overflow-hidden mt-5">
-          <motion.img
-            id="teste"
-            ref={moonRef}
-            className="max-h-[120rem]"
-            src={MoonAnimation}
-            alt="Moon Animation"
-            animate={{
-              opacity: 1,
-              translateY: -positions[rangeValue] + "rem",
-            }}
-            initial={{ opacity: 0, translateY: "-8rem" }}
-          />
-        </div>
-        <div className="flex justify-center mt-5 h-1/5">
-          <input
-            type="range"
-            name="afas"
-            id="aafs"
-            onChange={(e) => handleInputChange(e)}
-            value={rangeValue}
-            defaultValue={0}
-            min={0}
-            max={120}
-            step={20}
-          />
+      <div className="bg-gd-white flex justify-center items-center p-1 rounded-3xl md:rounded-[3.25rem] h-full w-full">
+        <div className="h-full w-full p-5 bg-main rounded-3xl md:rounded-[3rem] shadow-main ">
+          <div className="flex justify-center max-h-8 md:max-h-12 h-1/5">
+            <img src={LogoMarca} alt="Logo Marca" />
+          </div>
+          <div className="h-[17rem] flex flex-col overflow-hidden mt-5">
+            <motion.img
+              id="teste"
+              ref={moonRef}
+              className="max-h-[120rem]"
+              src={MoonAnimation}
+              alt="Moon Animation"
+              animate={{
+                opacity: 1,
+                translateY: -positions[rangeValue].y + "rem",
+              }}
+              initial={{ opacity: 0, translateY: "-8rem" }}
+            />
+          </div>
+          <div className="flex justify-center mt-5">
+            <div className="bg-gd-white flex justify-center items-center p-[0.18rem] rounded-full">
+              <input
+                type="range"
+                name="afas"
+                id="aafs"
+                onChange={(e) => handleInputChange(e)}
+                value={rangeValue}
+                defaultValue={0}
+                min={0}
+                max={120}
+                step={20}
+              />
+            </div>
+          </div>
+          <motion.p
+            className="text-center mt-4 text-white text-2xl uppercase"
+            animate={{ opacity: 1, translateY: 0 }}
+            initial={{ opacity: 0, translateY: "-2rem" }}
+          >
+            {positions[rangeValue].phase}
+          </motion.p>
         </div>
       </div>
     </main>
