c5cbc06295f64441438e68671eb0ee9adbc40f1b
mendesjar <ga0301@outlook.com>
2024-08-02 14:43:37 -0300

    feat: transition slider moon function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { ChangeEvent, useState } from "react";
 import LogoMarca from "./assets/logo-marca.svg";
 import MoonAnimation from "./assets/moon-phases-animation.svg";
 
 function App() {
+  const [rangeValue, setRangeValue] = useState<number>(0);
+  let idx: any = {
+    0: 0,
+    20: 17,
+    40: 34,
+    60: 51,
+    80: 68,
+    100: 85,
+    120: 102,
+  };
+
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    const value = Number(e.target.value);
+    setRangeValue(value);
+
+    const moon = document.getElementById("teste");
+    if (moon) {
+      const pos = idx[value];
+      moon.style.transform = `translateY(-${pos}rem)`;
+    }
+  }
+
   return (
     <main className="bg-blue-100 h-[100dvh] p-3">
       <div className="h-full w-full p-5 bg-main rounded-3xl md:rounded-[3rem] shadow-main border-2 border-opacity-65 border-white">
@@ -10,9 +33,23 @@ function App() {
         </div>
         <div className="h-[17rem] flex flex-col overflow-hidden mt-5">
           <img
-            className="moon max-h-[120rem] transition-all"
+            id="teste"
+            className="max-h-[120rem] transition ease-in-out duration-500"
             src={MoonAnimation}
             alt="Moon Animation"
+          />
+        </div>
+        <div className="flex justify-center mt-5">
+          <input
+            type="range"
+            name="afas"
+            id="aafs"
+            onChange={(e) => handleInputChange(e)}
+            value={rangeValue}
+            defaultValue={0}
+            min={0}
+            max={120}
+            step={20}
           />
         </div>
       </div>
