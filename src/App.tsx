e5ae632d7af0eb35ec9ffb287dd5d9e330f40312
mendesjar <ga0301@outlook.com>
2024-08-02 15:33:00 -0300

    feat: add motion framer animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import LogoMarca from "./assets/logo-marca.svg";
 import MoonAnimation from "./assets/moon-phases-animation.svg";
+import { motion } from "framer-motion";
 
 function App() {
   const [rangeValue, setRangeValue] = useState<number>(0);
-  let idx: any = {
+  const moonRef = useRef(null);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value);
+    const moon = document.getElementById("teste");
+    if (moon) {
+      moon.style.opacity = "0";
+    }
+    setRangeValue(value);
+  };
+  let positions: any = {
     0: 0,
     20: 17,
     40: 34,
@@ -14,7 +25,7 @@ function App() {
     120: 102,
   };
 
-  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+  /* function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
     const value = Number(e.target.value);
     setRangeValue(value);
 
@@ -23,23 +34,29 @@ function App() {
       const pos = idx[value];
       moon.style.transform = `translateY(-${pos}rem)`;
     }
-  }
+  } */
 
   return (
-    <main className="bg-blue-100 h-[100dvh] p-3">
+    <main className="bg-blue-950 h-dvh p-3">
       <div className="h-full w-full p-5 bg-main rounded-3xl md:rounded-[3rem] shadow-main border-2 border-opacity-65 border-white">
-        <div className="flex justify-center max-h-8 md:max-h-10">
+        <div className="flex justify-center max-h-8 md:max-h-12 h-1/5">
           <img src={LogoMarca} alt="Logo Marca" />
         </div>
         <div className="h-[17rem] flex flex-col overflow-hidden mt-5">
-          <img
+          <motion.img
             id="teste"
-            className="max-h-[120rem] transition ease-in-out duration-500"
+            ref={moonRef}
+            className="max-h-[120rem]"
             src={MoonAnimation}
             alt="Moon Animation"
+            animate={{
+              opacity: 1,
+              translateY: -positions[rangeValue] + "rem",
+            }}
+            initial={{ opacity: 0, translateY: "-8rem" }}
           />
         </div>
-        <div className="flex justify-center mt-5">
+        <div className="flex justify-center mt-5 h-1/5">
           <input
             type="range"
             name="afas"
