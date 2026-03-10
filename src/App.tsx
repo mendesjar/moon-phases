8a8dcf9ecce618190eb0504b566fea2987858585
mendesjar <ga0301@outlook.com>
2026-03-10 16:05:27 -0300

    feat: ajustar layout da página e adicionar componente LiveTime para exibir hora atual

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { LunarPhase, Moon } from "lunarphase-js";
 import { HemisphereHelper } from "./helpers";
+import { ReactNode, useEffect, useState } from "react";
+
+const LOCALE: string = "pt-BR";
 
 function ItemPhase({
   phase,
@@ -22,13 +25,44 @@ function ItemPhase({
           <p className="text-5xl">{emoji}</p>
           <div className="absolute bottom-0 -right-2">
             <span className="text-xs bg-white/75 text-black rounded-sm py-0.2 px-1.5">
-              {new Date(date).toLocaleString("en-US", { weekday: "short" })}
+              {new Date(date).toLocaleString(LOCALE, { weekday: "short" })}
             </span>
           </div>
         </div>
       </div>
     </div>
   );
+}
+
+function ItemDetails({ children }: { children: ReactNode }) {
+  return (
+    <div className="bg-blue-900 flex px-2.5 font-light py-1 rounded-md text-sm">
+      {children}
+    </div>
+  );
+}
+
+function LiveTime() {
+  const [time, setTime] = useState(
+    new Date().toLocaleTimeString(LOCALE, {
+      hour: "2-digit",
+      minute: "2-digit",
+    }),
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(
+        new Date().toLocaleTimeString(LOCALE, {
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+      );
+    }, 60 * 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <span>{time}</span>;
 }
 
 function App() {
@@ -54,9 +88,9 @@ function App() {
   };
 
   return (
-    <div className="p-5 flex flex-col items-center gap-y-6">
-      <div className="flex flex-col items-center gap-y-3">
-        <div className="flex gap-x-3 w-max mask-x-from-50% mask-x-to-96%">
+    <div className="p-5 flex flex-col items-center gap-y-6 justify-center min-h-screen">
+      <div className="flex flex-col items-center gap-y-8">
+        <div className="flex gap-x-3 w-full md:w-max mask-x-from-50% mask-x-to-96%">
           {dates.map((dt) => {
             const phase = Moon.lunarPhase(dt, { hemisphere: hemisphere.get() });
             const label = formatLabel(dt);
@@ -70,9 +104,31 @@ function App() {
             );
           })}
         </div>
-        <p className="text-md font-bold">{Moon.lunarPhase(new Date())}</p>
+        <div>
+          <p className="text-md font-bold text-center mb-2">
+            {Moon.lunarPhase(new Date())}
+          </p>
+          <div className="flex gap-x-2">
+            <ItemDetails>Today</ItemDetails>
+            <ItemDetails>
+              {String(hemisphere.get()) === "northern"
+                ? "Northern"
+                : "Southern"}
+            </ItemDetails>
+            <ItemDetails>
+              <LiveTime />
+            </ItemDetails>
+          </div>
+        </div>
+        <span className="bg-linear-90 to-transparent via-white from-transparent w-full h-px" />
+        <div className="bg-linear-130 from-white to-transparent p-px rounded-xl">
+          <div className="bg-blue-900 px-6 py-2 rounded-xl min-w-50">
+            <span className="text-xs">Distance:</span>
+            <p className="text-md">{Moon.lunarDistance().toFixed(3)} mi</p>
+          </div>
+        </div>
       </div>
-      <p className="fixed right-10 bottom-1/3 -translate-y-1/2 -rotate-90 origin-right font-extrabold">
+      <p className="fixed right-10 bottom-1/3 -translate-y-1/2 -rotate-90 origin-right font-light">
         MOON PHASES
       </p>
     </div>
